1a16191fb223cdee2434758e472f12c59529b70e
Mulualem2 <mulualemasfaw2321@gmail.com>
2022-12-19 20:26:39 +0000

    advanced task

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,5 +1,6 @@
 export default function createIteratorObject(report) {
   const emps = [];
+  /* eslint-disable */
 	for (const dep of Object.keys(report.allEmployees)) {
 		for (const emp of report.allEmployees[dep]) {
       emps.push(emp);
