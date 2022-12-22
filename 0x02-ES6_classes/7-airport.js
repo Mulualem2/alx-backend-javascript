9dfdd409c9507179333cd55f2c924ed620b9a785
Mulualem2 <mulualemasfaw2321@gmail.com>
2022-12-22 17:44:21 +0000

    task 7

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,11 @@
+export default class Airport {
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
+  }
+
+  // method
+  toString() {
+    return `[object ${this._code}]`;
+  }
+}
